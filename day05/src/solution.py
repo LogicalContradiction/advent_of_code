7333a684c0ab8a6958bf0367d596bc880bc53312
Logical Contradiction <chrisgiaramita@yahoo.com>
2022-12-07 18:18:55 -0500

    Day 5, solution 1 posted.

---
+++ day05/src/solution.py
@@ -0,0 +1,141 @@
+from pathlib import Path
+from collections import deque
+
+
+
+def readInput(input_filename):
+	filepath = Path(__file__).parent / input_filename
+	with open(filepath, "r", encoding="utf8", newline=None) as file:
+			data = file.readlines()
+	return data
+
+def decode_starting_pos(init_pos):
+	"""Takes the data and creates queues based on the starting location of each crate.
+	
+	Parameters:
+		data (list<str>): List of strings representing the inital stacks of crates.
+		
+	Returns:
+		list<deque>: List of deque objects where each deque represents the inital stacks."""
+	result = []
+	num_stacks = len(init_pos[-1]) // 4	#each stack uses 3 characters to represent it and a space separator
+										#The last stack uses a \n separator
+	#append empty lists for stacks
+	for index in range(num_stacks):
+		result.append(deque())
+	#now fill the stacks
+	for row in init_pos[:-1]:	#last line has the stack numbers, so ignore it
+		curr_stack_index = 0
+		#go through each position and add it to the deque
+		for crate_index in range(1, len(row), 4):
+			crate = row[crate_index]
+			if crate != " ":
+				result[curr_stack_index].appendleft(crate)
+			curr_stack_index += 1
+	return result
+
+def decode_single_movement(move):
+	"""Takes a single movement representation and converts it to a usable format.
+	
+	Parameters:
+		move (str): A string representation of a sigle movement.
+		
+	Returns:
+		int, int, int: Represents this movement. Format: (source, destination, amount)."""
+	data = move.split(" ")
+	amount = int(data[1])
+	source = int(data[3])
+	dest = int(data[5])
+	return source, dest, amount
+
+def decode_all_movements(all_moves):
+	"""Takes the text of the crate movements and converts it to a usable format.
+	
+	Paramters:
+		all_moves (list<str>): A list of string representations of the crate movements.
+		
+	Returns:
+		list<tuples<int, int, int>>: A list of tuples representing the moves that will be made. Format: (source, destination, amount)."""
+	result = []
+	for move in all_moves:
+		result.append(decode_single_movement(move))
+	return result
+
+def process_single_move(move, stacks):
+	"""Moves a crate from its source to its destination.
+	
+	Parameters:
+		move (tuple<int,int,int>): Represents the move that needs to be executed.
+		stacks (list<deque>): The current state of the stacks.
+		
+	Returns:
+		list (deque<str>): The same list as stacks."""
+	source, dest, amount = move
+	#subtract 1 from source and dest to convert from stack num to stack list index
+	source_index = source - 1
+	dest_index = dest - 1
+	for crate_num in range(amount):
+		crate = stacks[source_index].pop()
+		stacks[dest_index].append(crate)
+	return stacks
+
+def get_top_crates_message(stacks):
+	"""Gets the top crate from each stack and combines them to generate the solution message.
+	
+	Parameters:
+		stacks (list<deque<str>>): Represents the state of the stacks.
+		
+	Returns:
+		str: The top elements of each stack, combined into one string."""
+	result = ""
+	for stack in stacks:
+		crate = stack[-1]
+		result = result + crate
+	return result
+
+def process_all_moves(moves, stacks):
+	"""Moves all the crates in stacks accourding to the moves.
+	
+	Parameters:
+		moves (int, int, int): Represents the movements of the crates.
+		stacks (list<deque<str>>): Represents the current state of the stacks of crates.
+		
+	Returns:
+		list<duque<str>>: The final state of the stacks. This is the same reference as stacks."""
+	for move in moves:
+		process_single_move(move, stacks)
+	return stacks
+
+def get_div_point_index_of_data(data):
+	"""Calculates the index of the split point between the stack info and move list. Split point is a single string of just newline.
+	
+	Parameters:
+		data (list<str>): List of strings that were read from the input.
+	
+	Returns:
+		int: The integer of the single newline which is the breakpoint between the stacks info and the move list."""
+	for index in range(len(data)):
+		if data[index] == "\n":
+			return index
+	return -1
+
+def do_simulation(data):
+	"""Simulates moving the crates based off of the data.
+	
+	Parameters:
+		data (list<str>): The input data to run this simulation based on.
+		
+	Returns:
+		str: A string containing the crates on top of each stack."""
+	div_point = get_div_point_index_of_data(data)
+	stacks = decode_starting_pos(data[:div_point])
+	moves = decode_all_movements(data[div_point+1:])
+	process_all_moves(moves, stacks)
+	result = get_top_crates_message(stacks)
+	return result
+
+
+def run_solution_1(filename):
+	data = readInput(filename)
+	message = do_simulation(data)
+	print(f"You need to give the Elves the message '{message}'.")
---
+++ day05/tests/tests.py
@@ -0,0 +1,63 @@
+import unittest
+from pathlib import Path
+from collections import deque
+
+from src import solution
+
+
+class AoC_2022_Puzzle_5_Tests(unittest.TestCase):
+
+	def setUp(self):
+		filepath = Path(__file__).parent / "test_input.txt"
+		with open(filepath, "r", encoding="utf8", newline=None) as file:
+			self.data = file.readlines()
+
+	def test_decode_starting_pos(self):
+		init_stack = self.data[:4]
+		exp_res_stack1 = deque(["Z", "N"])
+		exp_res_stack2 = deque(["M", "C", "D"])
+		exp_res_stack3 = deque(["P"])
+		result = solution.decode_starting_pos(init_stack)
+
+		self.assertEqual(result[0], exp_res_stack1)
+		self.assertEqual(result[1], exp_res_stack2)
+		self.assertEqual(result[2], exp_res_stack3)
+	
+	def test_decode_single_movement(self):
+		move1 = "move 1 from 2 to 1"
+		move2 = "move 1 from 2 to 1\n"
+		self.assertEqual(solution.decode_single_movement(move1), (2, 1, 1))
+		self.assertEqual(solution.decode_single_movement(move2), (2, 1, 1))
+
+	def test_decode_all_movements(self):
+		exp_res = [(2,1,1), (1,3,3), (2,1,2), (1,2,1)]
+		result = solution.decode_all_movements(self.data[5:])
+		self.assertEqual(exp_res, result)
+
+	def test_process_single_move(self):
+		stacks = [deque(["A", "B", "C"]), deque(["D", "E", "F"])]
+		move = (1, 2, 2)
+		exp_result = [deque(["A"]), deque(["D", "E", "F", "C", "B"])]
+		result = solution.process_single_move(move, stacks)
+		self.assertEqual(exp_result, result)
+
+	def test_get_top_crates_message(self):
+		stacks = [deque(["A", "B", "C"]), deque(["D", "E", "F"])]
+		exp_res = "CF"
+		result = solution.get_top_crates_message(stacks)
+		self.assertEqual(exp_res, result)
+
+	def test_process_all_moves(self):
+		stacks = [deque(["Z", "N"]), deque(["M", "C", "D"]), deque(["P"])]
+		moves = [(2,1,1), (1,3,3), (2,1,2), (1,2,1)]
+		exp_res = [deque(["C"]), deque(["M"]), deque(["P", "D", "N", "Z"])]
+		result = solution.process_all_moves(moves, stacks)
+		self.assertEqual(exp_res, result)
+
+	def test_get_div_point_index_of_data(self):
+		self.assertEqual(solution.get_div_point_index_of_data(self.data), 4)
+
+	def test_do_simulation(self):
+		exp_res = "CMZ"
+		result = solution.do_simulation(self.data)
+		self.assertEqual(exp_res, result)
