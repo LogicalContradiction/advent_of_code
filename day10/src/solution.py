60fc19e0fd50ba3d20223c5e6d68bec077093dfd
Logical Contradiction <chrisgiaramita@yahoo.com>
2022-12-23 19:53:53 -0500

    Day 10, solution 2 posted.

--- day10/src/solution.py
+++ day10/src/solution.py
@@ -87,9 +87,83 @@ def get_sum_of_certain_signal_strengths_from_instructions(instructions, cycle_nu
 	strength_sum = caluclate_sum_of_signal_strength(signal_strengths, cycle_nums)
 	return strength_sum
 
+def convert_signal_strengths_to_register_value(signal_strengths):
+	""""""
+	register_values = []
+	#no cycle 0, so append placeholder
+	register_values.append(0)
+	for index in range(1, len(signal_strengths), 1):
+		strength = signal_strengths[index]
+		register_values.append(strength // index)
+	return register_values
+
+def get_min_max_sprite_pos(sprite_size, center_index):
+	"""Determines the minimum and maximum index this sprite will occupy.
+	
+	Parameters:
+		sprite_size (int): The number of pixels this sprite is.
+		center_index (int): The middle index of the sprite.
+		
+	Returns:
+		int, int: The minimum and maximum index of the sprite, respecitvely"""
+	sprite_size_half = sprite_size // 2
+	min_index = center_index - sprite_size_half
+	max_index = center_index + sprite_size_half
+	return min_index, max_index
+
+def is_sprite_on_screen(reg_value, curr_cycle_num, sprite_size, screen_width):
+	"""Determines if the sprite will be visible when the pixel is drawn.
+	
+	Parameters:
+		reg_value (int): The value being held by the register during this cycle.
+		curr_cycle_num (int): The current cycle number.
+		sprite_size (int): The number of pixels the sprite is
+		screen_width (int): The width (in pixels) of the screen
+		
+	Returns:
+		bool: True if the sprite will be visible when the pixel is drawn, otherwise False."""
+	#current sprite position is centered on the value in the register
+	min_index, max_index = get_min_max_sprite_pos(sprite_size, reg_value)
+	curr_pixel_index = (curr_cycle_num-1) % screen_width #the index of the pixel being drawn is one less than the cycle number
+	return curr_pixel_index >= min_index and curr_pixel_index <= max_index #pixel is visible if it's on or between the min and max
+
+def generate_image(screen_width, reg_values, sprite_size):
+	"""Generates the screen image based on the register values.
+	
+	Parameters:
+		screen_width (int): The width (in pixels) of the screen.
+		screen_height (int): The height (in pixels) of the screen.
+		reg_values (list<int>): The value of the register at the start of each cycle.
+		sprite_size (int): The size (in pixels) of the sprite.
+		
+	Returns:
+		str: The image that will be printed."""
+	image = ""
+	cycle_num = 0
+	while cycle_num < len(reg_values)-1:
+		cycle_num += 1
+		reg_value = reg_values[cycle_num]
+		if is_sprite_on_screen(reg_value, cycle_num, sprite_size, screen_width):
+			image = image + "#"
+		else:
+			image = image + "."
+		if cycle_num % screen_width == 0:
+			image = image + "\n"
+	return image
+
+
 
 def run_solution_1(filename):
 	instructions = readInput(filename)
 	cycle_nums = [20, 60, 100, 140, 180, 220]
 	strength_sum = get_sum_of_certain_signal_strengths_from_instructions(instructions, cycle_nums)
 	print(f"The sum of the six signal strengths is {strength_sum}.")
+
+def run_solution_2(filename):
+	instructions = readInput(filename)
+	signal_strengths = simulate_instructions(instructions)
+	reg_values = convert_signal_strengths_to_register_value(signal_strengths)
+	screen_width = 40
+	sprite_size = 3
+	image = generate_image(screen_width, reg_values, sprite_size)
+	print(f"The image generated:\n{image}\n")
--- day10/tests/tests.py
+++ day10/tests/tests.py
@@ -41,3 +41,52 @@ class AoC_2022_Puzzle_10_Tests(unittest.TestCase):
 		result = solution.get_sum_of_certain_signal_strengths_from_instructions(self.data, cycle_nums)
 
 		self.assertEqual(exp_result, result)
+
+	def test_convert_signal_strengths_to_register_value(self):
+		strengths = [0,1,4,9,16,25,36,49,64,81,100]
+		exp_results = [0,1,2,3,4,5,6,7,8,9,10]
+		results = solution.convert_signal_strengths_to_register_value(strengths)
+
+		self.assertEqual(exp_results, results)
+
+	def test_get_min_max_sprite_pos(self):
+		center_index = 10
+		sprite_size = 3
+		exp_result = (9, 11)
+		result = solution.get_min_max_sprite_pos(sprite_size, center_index)
+
+		self.assertEqual(exp_result, result)
+
+	def test_is_sprite_on_screen(self):
+		reg_value = 10
+		cycle_num_1 = 10
+		cycle_num_2 = 11
+		cycle_num_3 = 12
+		cycle_num_4 = 13
+		sprite_size = 3
+		screen_width = 40
+		result1 = solution.is_sprite_on_screen(reg_value, cycle_num_1, sprite_size, screen_width)
+		result2 = solution.is_sprite_on_screen(reg_value, cycle_num_2, sprite_size, screen_width)
+		result3 = solution.is_sprite_on_screen(reg_value, cycle_num_3, sprite_size, screen_width)
+		result4 = solution.is_sprite_on_screen(reg_value, cycle_num_4, sprite_size, screen_width)
+
+		self.assertTrue(result1)
+		self.assertTrue(result2)
+		self.assertTrue(result3)
+		self.assertFalse(result4)
+
+	def test_generate_image(self):
+		sprite_size = 3
+		signal_strengths = solution.simulate_instructions(self.data)
+		reg_values = solution.convert_signal_strengths_to_register_value(signal_strengths)
+		screen_width = 40
+		exp_result = """##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....
+"""
+		result = solution.generate_image(screen_width, reg_values, sprite_size)
+
+		self.assertEqual(exp_result, result)
